4fdb36144265e870adfd68fb266e80187ba2acdb
rendhive <rendhivecode@gmail.com>
2025-11-23 03:13:40 +0700

    [DOC]alihkan callendar py

---
+++ 10. Mencetak Alih-alih Mengembalikan Kalender.py
@@ -0,0 +1,7 @@
+import calendar
+
+year = 2023
+cal = calendar.TextCalendar()
+print(cal.formatyear(year))
+# Fungsi: Mencetak kalender tahun dalam format teks yang terstruktur.
+# Kondisi: Ketika Anda ingin menampilkan kalender di konsol tanpa menyimpannya.
